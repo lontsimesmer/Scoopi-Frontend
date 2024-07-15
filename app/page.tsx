1fc958c14d0a138cb0093b165cea6595fad780c1
lontsimesmer <tchoupounathanael@gmail.com>
2024-07-15 12:44:46 +0100

    add the counter exercise

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
+  const [count, setCount] = useState(0);
   return (
     <main>
       <div className="bg-gray-900 flex text-white items-center h-screen justify-center flex-col">
         <h1 className="text-xl font-bold">Test app</h1>
-        <p>Now you can start coding</p>
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>Click me</button>
       </div>
     </main>
   );
